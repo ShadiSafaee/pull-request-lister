be7e8848fd128522b20b03cf927553a56ec06f58
Barath Vignarajah <moneyinthesky@gmail.com>
2023-03-22 07:49:54 +0000

    Fix sorting of PRs

--- prs-by-repo.js
+++ prs-by-repo.js
@@ -10,21 +10,38 @@ async function run() {
 
 function outputPrsByRepo(prByRepoAndAuthor) {
   repos.forEach(repo => {
-    let output = authors.map(author => ({
-      name: author.name,
-      reviewRequested: doesLabelExist(prByRepoAndAuthor[repo][author.githubUser], 'review requested'),
-      reviewed: doesLabelExist(prByRepoAndAuthor[repo][author.githubUser], 'reviewed'),
-      url: getUrl(prByRepoAndAuthor[repo][author.githubUser])
-    }))
-    .sort((a, b) => a.name < b.name ? -1 : 1)
-    .sort((a, b) => !a.reviewed && b.reviewed ? -1 : 1)
-    .sort((a, b) => a.reviewRequested && !b.reviewRequested ? -1 : 1)
-    .sort((a, b) => a.url !== '<missing>' && b.url === '<missing>' ? -1 : 1);
+    let missingAuthors = authors.filter(author => !prByRepoAndAuthor[repo][author.githubUser]);
+    let allPrs = authors
+      .filter(author => !missingAuthors.includes(author))
+      .map(author => authorToOutputObj(prByRepoAndAuthor[repo][author.githubUser], author));
+
+    let missing = missingAuthors.map(authorToMissingPr);
+    let reviewed = allPrs.filter(pr => pr.reviewed);
+    let notReviewedButNotRequested = allPrs.filter(pr => !pr.reviewed && !pr.reviewRequested);
+    let notReviewedAndRequested = allPrs.filter(pr => !pr.reviewed && pr.reviewRequested);
 
     console.log();
     console.log(`Pull Requests for ${repo}`);
-    console.table(output);
+    console.table([...notReviewedAndRequested, ...notReviewedButNotRequested, ...reviewed, ...missing]);
   })
+}
+
+function authorToOutputObj(pr, author) {
+  return {
+    name: author.name,
+    reviewRequested: doesLabelExist(pr, 'review requested'),
+    reviewed: doesLabelExist(pr, 'reviewed'),
+    url: getUrl(pr)
+  }
+}
+
+function authorToMissingPr(author) {
+  return {
+    name: author.name,
+    reviewRequested: '-',
+    reviewed: '-',
+    url: '<missing>'
+  };
 }
 
 run();
